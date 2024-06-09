c0bc90ed6e859148ca5410625bc0284aeceb2347
Brian Wang <brianwang7102@gmail.com>
2024-06-09 19:21:46 -0400

    new: website layout completed

--- WTWR/src/components/ItemCard/ItemCard.jsx
+++ WTWR/src/components/ItemCard/ItemCard.jsx
@@ -1,5 +1,12 @@
-function ItemCard() {
+import './ItemCard.css'
 
+function ItemCard(props) {
+    return (
+        <div className="item-card">
+            <h1 className='item-card__name'>{props.name}</h1>
+            <img src={props.link} alt={props.name} className='item-card__image' />
+        </div>
+    )
 }
 
 export default ItemCard;
--- WTWR/src/components/Main/Main.jsx
+++ WTWR/src/components/Main/Main.jsx
@@ -1,12 +1,27 @@
+import './Main.css'
 import WeatherCard from "../WeatherCard/WeatherCard"
 import ItemCard from "../ItemCard/ItemCard"
-import './Main.css'
+import defaultClothingItems from '../../utils/defaultClothing.js'
+
 
 function Main(props) {
     return (
         <>
             <WeatherCard isNight={props.isNight} temperature={props.temperature} weather={props.weather}></WeatherCard>
-            <p className="main__blurb">Today is {props.temperature} ° F / You may want to wear:</p>
+            <p className="main__blurb">
+                Today is {props.temperature} ° F / You may want to wear:
+            </p>
+            <ul className="main__card-list">
+            {
+                defaultClothingItems.map(item => {
+
+                    return (
+                        <ItemCard key={item._id} name={item.name} link={item.link}></ItemCard>
+                    )
+                    
+                })
+            }
+            </ul>
         </>
     )
 }
