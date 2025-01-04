d26bec1ec900ebce77f79395341a4d1dab352ef2
Brian Wang <brianwang7102@gmail.com>
2025-01-04 17:49:06 -0500

    refactor: changed form elememts to use controlled components

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -8,25 +8,26 @@ import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
 
 export function LoginModal(props) {
-    const emailRef = useRef();
-    const passwordRef = useRef();
     const [ validPassword, setValidPassword ] = useState(true);
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        props.handleAuthorization(emailRef.current.value, passwordRef.current.value)
+        props.handleAuthorization(email, password)
         .catch((err) => {
             console.error(err);
             setValidPassword(false);
         })
-    } 
+    }
 
     return (
         <ModalWithForm handleCloseButtonClick={props.handleCloseButtonClick} handleOnSubmit={handleSubmit} title={"Log in"} buttonStyle={'login-modal__hide-default-button'}>
             <label htmlFor="email" className='modal__label'>email</label>
-            <input type="email" id="email" placeholder='email' ref={emailRef} className='modal__input'/>
+            <input type="email" id="email" placeholder='email' value={email} onChange={(evt) => { setEmail(evt.target.value) }} className='modal__input'/>
             <label htmlFor="password" className={`modal__label ${!validPassword && 'modal__invalid'}`}>password</label>
-            <input type="password" id="password" placeholder='password' ref={passwordRef} className={`modal__input ${!validPassword && 'modal__invalid'}`}/>
+            <input type="password" id="password" placeholder='password' value={password} onChange={(evt) => { setPassword(evt.target.value) }} className={`modal__input ${!validPassword && 'modal__invalid'}`}/>
             <div className='login-modal__submit-bar'>
                 <button className='login-modal__submit project__button' type='submit'> Log in</button>
                 <button className='project__contrast-button' onClick={props.handleToggleClick}>or Sign Up</button>
--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 import '../Modal/Modal.css';
 import '../ModalWithForm/ModalWithForm.css';
@@ -7,26 +7,27 @@ import '../../assets/projectStyles.css';
 import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
 export function RegisterModal(props) {
-    const nameRef = useRef();
-    const avatarRef = useRef();
-    const emailRef = useRef();
-    const passwordRef = useRef();
+
+    const [ name, setName ] = useState("");
+    const [ avatar, setAvatar ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
 
     function handleSubmit(evt) {
         evt.preventDefault();
-        props.handleRegistration(nameRef.current.value, avatarRef.current.value, emailRef.current.value, passwordRef.current.value);
+        props.handleRegistration(name, avatar, email, password);
     }
 
     return (
         <ModalWithForm handleCloseButtonClick={props.handleCloseButtonClick} title={"Register"} handleOnSubmit={handleSubmit} buttonStyle={'register-modal__hide-default-button'}>
             <label htmlFor="email" className='modal__label'>Email*</label>
-            <input type="email" id="email" className='modal__input' placeholder='email' ref={emailRef}/>
+            <input type="email" id="email" className='modal__input' placeholder='email' value={email} onChange={(evt) => { setEmail(evt.target.value)}}/>
             <label htmlFor="password" className='modal__label'>Password*</label>
-            <input type="password" id="password" className='modal__input' placeholder='password' ref={passwordRef}/>
+            <input type="password" id="password" className='modal__input' placeholder='password' value={password} onChange={(evt) => { setPassword(evt.target.value)}}/>
             <label htmlFor="name" className='modal__label'>Name*</label>
-            <input type="text" id="name" className='modal__input' placeholder='name' ref={nameRef}/>
+            <input type="text" id="name" className='modal__input' placeholder='name' value={name} onChange={(evt) => { setName(evt.target.value)}}/>
             <label htmlFor="avatar" className='modal__label'>AvatarURL*</label>
-            <input type="url" id="avatar" className='modal__input' placeholder='avatar URL' ref={avatarRef}/>
+            <input type="url" id="avatar" className='modal__input' placeholder='avatar URL' value={avatar} onChange={(evt) => { setAvatar(evt.target.value)}}/>
             <div className='login-modal__submit-bar register-modal__bottom-buttons'>
                 <button className='login-modal__submit project__button' type='submit'>Sign Up</button>
                 <button className='project__contrast-button' onClick={props.handleToggleClick}>or Log In</button>
