71e6b32a29d5d3e911c157f9846d316848eebd88
Brian Wang <brianwang7102@gmail.com>
2025-01-04 17:16:44 -0500

    refactor: update register modal and log in modal to use modalwithform component

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -4,6 +4,7 @@ import '../Modal/Modal.css';
 import '../ModalWithForm/ModalWithForm.css';
 import './LoginModal.css';
 import '../../assets/projectStyles.css';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
 
 export function LoginModal(props) {
@@ -21,19 +22,15 @@ export function LoginModal(props) {
     } 
 
     return (
-        <div className="modal form-modal">
-            <button onClick={props.handleCloseButtonClick} className="modal__close-button form-modal__close-button"></button>
-            <h1 className="form-modal__title">Log In</h1>
-            <form id="login-in" className='form-modal__form login-modal__form' onSubmit={handleSubmit}>
-                <label htmlFor="email" className='modal__label'>email</label>
-                <input type="email" id="email" placeholder='email' ref={emailRef} className='modal__input'/>
-                <label htmlFor="password" className={`modal__label ${!validPassword && 'modal__invalid'}`}>password</label>
-                <input type="password" id="password" placeholder='password' ref={passwordRef} className={`modal__input ${!validPassword && 'modal__invalid'}`}/>
-                <div className='login-modal__submit-bar'>
-                    <button className='login-modal__submit project__button' type='submit'> Log in</button>
-                    <button className='project__contrast-button' onClick={props.handleToggleClick}>or Sign Up</button>
-                </div>
-            </form>
-        </div>
+        <ModalWithForm handleCloseButtonClick={props.handleCloseButtonClick} handleOnSubmit={handleSubmit} title={"Log in"} buttonStyle={'login-modal__hide-default-button'}>
+            <label htmlFor="email" className='modal__label'>email</label>
+            <input type="email" id="email" placeholder='email' ref={emailRef} className='modal__input'/>
+            <label htmlFor="password" className={`modal__label ${!validPassword && 'modal__invalid'}`}>password</label>
+            <input type="password" id="password" placeholder='password' ref={passwordRef} className={`modal__input ${!validPassword && 'modal__invalid'}`}/>
+            <div className='login-modal__submit-bar'>
+                <button className='login-modal__submit project__button' type='submit'> Log in</button>
+                <button className='project__contrast-button' onClick={props.handleToggleClick}>or Sign Up</button>
+            </div>
+        </ModalWithForm>
     )
 }
--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -4,6 +4,7 @@ import '../Modal/Modal.css';
 import '../ModalWithForm/ModalWithForm.css';
 import './RegisterModal.css';
 import '../../assets/projectStyles.css';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
 export function RegisterModal(props) {
     const nameRef = useRef();
@@ -17,23 +18,19 @@ export function RegisterModal(props) {
     }
 
     return (
-        <div className="modal form-modal">
-            <button onClick={props.handleCloseButtonClick} className="modal__close-button form-modal__close-button"></button>
-            <h1 className="form-modal__title">Register</h1>
-            <form id="login-in" className='form-modal__form register-modal__form' onSubmit={handleSubmit}>
-                <label htmlFor="email" className='modal__label'>Email*</label>
-                <input type="email" id="email" className='modal__input' placeholder='email' ref={emailRef}/>
-                <label htmlFor="password" className='modal__label'>Password*</label>
-                <input type="password" id="password" className='modal__input' placeholder='password' ref={passwordRef}/>
-                <label htmlFor="name" className='modal__label'>Name*</label>
-                <input type="text" id="name" className='modal__input' placeholder='name' ref={nameRef}/>
-                <label htmlFor="avatar" className='modal__label'>AvatarURL*</label>
-                <input type="url" id="avatar" className='modal__input' placeholder='avatar URL' ref={avatarRef}/>
-                <div className='login-modal__submit-bar register-modal__bottom-buttons'>
-                    <button className='login-modal__submit project__button' type='submit'>Sign Up</button>
-                    <button className='project__contrast-button' onClick={props.handleToggleClick}>or Log In</button>
-                </div>
-            </form>
-        </div>
+        <ModalWithForm handleCloseButtonClick={props.handleCloseButtonClick} title={"Register"} handleOnSubmit={handleSubmit} buttonStyle={'register-modal__hide-default-button'}>
+            <label htmlFor="email" className='modal__label'>Email*</label>
+            <input type="email" id="email" className='modal__input' placeholder='email' ref={emailRef}/>
+            <label htmlFor="password" className='modal__label'>Password*</label>
+            <input type="password" id="password" className='modal__input' placeholder='password' ref={passwordRef}/>
+            <label htmlFor="name" className='modal__label'>Name*</label>
+            <input type="text" id="name" className='modal__input' placeholder='name' ref={nameRef}/>
+            <label htmlFor="avatar" className='modal__label'>AvatarURL*</label>
+            <input type="url" id="avatar" className='modal__input' placeholder='avatar URL' ref={avatarRef}/>
+            <div className='login-modal__submit-bar register-modal__bottom-buttons'>
+                <button className='login-modal__submit project__button' type='submit'>Sign Up</button>
+                <button className='project__contrast-button' onClick={props.handleToggleClick}>or Log In</button>
+            </div>
+        </ModalWithForm>
     )
 }
